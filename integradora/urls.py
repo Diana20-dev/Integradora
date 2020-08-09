d146e5bb3d9f413972b47a6ba686b8f51a2a7009
Diana Karen Parra Sosa <dianaparra092@gmail.com>
2020-08-09 16:24:09 -0500

    Incio de sesión

--- integradora/urls.py
+++ integradora/urls.py
@@ -21,4 +21,8 @@ urlpatterns = [
     path('', include('core.urls')),
     path('pages/', include(pages_patterns)),  
     path('admin/', admin.site.urls),
+
+    #paths de autenticación 
+    path('accounts/', include('django.contrib.auth.urls')),
+    
 ]
