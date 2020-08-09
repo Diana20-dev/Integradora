36d7e2fefe13a03a54f35be40d4e8a67b057cb93
Diana Karen Parra Sosa <dianaparra092@gmail.com>
2020-08-09 01:08:16 -0500

    Agregando estilos

--- pages/models.py
+++ pages/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from ckeditor.fields import RichTextField
 
 class Page(models.Model):
-    title = models.CharField(verbose_name="Título", max_length=200)
-    content = RichTextField(verbose_name="Contenido")
-    order = models.SmallIntegerField(verbose_name="Orden", default=0)
+    title = models.CharField(verbose_name="Nombre del empleo", max_length=200)
+    content = RichTextField(verbose_name="Descripción del empleo")
+    order = models.SmallIntegerField(verbose_name="Orden de publicación", default=0)
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
 
